114d7f58f8318ce6f36fc013b1ed03fc45657f69
jaqarrick <jcarrick@protonmail.com>
2023-10-05 01:23:41 -0400

    remove logger comments

--- server/logger.js
+++ server/logger.js
@@ -3,15 +3,15 @@ const winston = require('winston');
 const { createLogger, format, transports } = winston;
 
 module.exports = createLogger({
-  level: 'info', // Set the log level as needed
+  level: 'info',
   format: format.combine(
-    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Customize the timestamp format
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
     format.printf(({ timestamp, level, message }) => {
       return `[${timestamp}] ${level}: ${message}`;
     }),
   ),
   transports: [
-    new transports.Console(), // Log to the console
-    new transports.File({ filename: 'error.log' }), // Log to a file (customize the filename)
+    new transports.Console(),
+    new transports.File({ filename: 'error.log' }),
   ],
 });
